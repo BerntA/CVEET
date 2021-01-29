6b36f244e9831790e479c9353851e6a8d9e6655b
Bernt A. Eide <bernta1@msn.com>
2021-01-29 14:51:19 +0100

    - Added code to convert the annotated scripts to TF record format. Added better train-test split logic so that I can get a decent amount of all vehicle types for both cases.

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,18 @@ def show_image(img, grayscale=False, file=None, size=(12,8)):
         plt.savefig('../images/temp/{}.png'.format(file))
     
     plt.show()
+
+def parseBBOXFile(file):
+    with open(file, 'r') as f:
+        content = []
+        for i, line in enumerate(f):
+            if i <= 12: # Skip these lines.
+                continue
+            line = line.strip().lower().replace('\n', '').replace('\r', '').replace('\t', '')
+            line = line.replace('</object>', '')
+            content.append(line)
+        return ''.join(content[:-1]).split('<object>')[1:] # Return content - last line.
+    
+def getTagValue(text, tag):
+    start, end = text.find('<{}>'.format(tag)), text.find('</{}>'.format(tag))
+    return text[(start+len(tag)+2):end]

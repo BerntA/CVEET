3ec975ed51d92c734087123d77b941732eafa267
Bernt A. Eide <bernta1@msn.com>
2021-03-23 13:40:30 +0100

    - Added hyperparameter, grid search tuning stuff.

--- src/transfer-learning.py
+++ src/transfer-learning.py
@@ -1,8 +1,9 @@
 import os
+import sys
+import time
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import confusion_matrix
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
@@ -10,13 +11,20 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow.keras.backend as K
 from tensorboard.plugins.hparams import api as hp
-from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras import layers
+from tensorflow.keras.optimizers import RMSprop, Adam, SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.layers import Dense, Dropout, InputLayer
+from tensorflow.keras.regularizers import l1, l2
+from sklearn.metrics import confusion_matrix
 
 cfg = tf.compat.v1.ConfigProto()
 cfg.gpu_options.allow_growth = True
 sess = tf.compat.v1.Session(config=cfg)
+
+HUB_URL = "https://tfhub.dev/google/imagenet/mobilenet_v3_large_100_224/feature_vector/5" # URL for feat. vec. (pre-trained mdl)
+BATCH_SIZE = 16
+TARGET_SIZE = (224, 224) # Model specific, see TF hub page for details.
 
 """
 Move raw collected data into the correct hierarchy:
@@ -83,7 +91,7 @@ def generateFigures(mdl, hist, y, y_pred, size=(8,6)):
     finally:
         plt.show()
 
-def createModel(mdlname, model_hub_url, BATCH_SIZE, SIZE):
+def createModel(mdlname, batch_size, epochs=4, optimizer=RMSprop, learn_rate=0.001, dropout=0.2, label_smoothing=0.1, regularizer=l2, regularizer_value=0.0001, traintune=False, gridsearch=False):
     train = ImageDataGenerator(
         rescale = 1.0/255.0, 
         rotation_range = 40, 
@@ -102,9 +110,9 @@ def createModel(mdlname, model_hub_url, BATCH_SIZE, SIZE):
     train_gen = train.flow_from_directory(
         "../images/train/", 
         color_mode="rgb", 
-        batch_size = BATCH_SIZE, 
+        batch_size = batch_size, 
         class_mode="categorical", 
-        target_size = SIZE, 
+        target_size = TARGET_SIZE, 
         interpolation="bilinear",
         shuffle=False
     )
@@ -112,58 +120,126 @@ def createModel(mdlname, model_hub_url, BATCH_SIZE, SIZE):
     test_gen = test.flow_from_directory(
         "../images/test/", 
         color_mode="rgb", 
-        batch_size = BATCH_SIZE, 
+        batch_size = batch_size, 
         class_mode="categorical", 
-        target_size = SIZE, 
+        target_size = TARGET_SIZE, 
         interpolation="bilinear",
         shuffle=False
     )
 
     # Load Pre-Trained Model, and freeze the weights.
     # Define the last layer of the CNN, a fully connected layer.
-    print("Creating model...")
+    if not gridsearch:
+        print("Creating model...")
+
     model = tf.keras.Sequential([
-        tf.keras.layers.InputLayer(input_shape=SIZE+(3,)),
-        hub.KerasLayer(model_hub_url, trainable=False),
-        tf.keras.layers.Dropout(rate=0.2),
-        tf.keras.layers.Dense(
-            train_gen.num_classes,
-            kernel_regularizer=tf.keras.regularizers.l2(0.0001),
-            activation='softmax'
-        )
+        InputLayer(input_shape=TARGET_SIZE+(3,)),
+        hub.KerasLayer(HUB_URL, trainable=traintune),
+        Dropout(rate=dropout),
+        Dense(train_gen.num_classes, kernel_regularizer=regularizer(regularizer_value), activation='softmax')
     ])
-    model.build((None,)+SIZE+(3,))
-    model.summary()
+    model.build((None,)+TARGET_SIZE+(3,))
+    if not gridsearch:
+        model.summary()
     model.compile(
-        optimizer = RMSprop(lr=0.001), 
-        loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=0.1), 
+        optimizer = optimizer(learning_rate=learn_rate), 
+        loss = CategoricalCrossentropy(from_logits=True, label_smoothing=label_smoothing), 
         metrics = ['accuracy']
     )
 
     # Train the model on our data.
-    print("Training model...")
+    if not gridsearch:
+        print("Training model...")
+
     hist = model.fit(
         train_gen,
         validation_data = test_gen,
         validation_steps = (test_gen.samples // test_gen.batch_size),
         steps_per_epoch = (train_gen.samples // train_gen.batch_size),    
-        epochs = 5,
+        epochs = epochs,
         shuffle = False
     ).history
 
-    print("Evaluating model...")
+    if not gridsearch:
+        print("Evaluating model...")
+
     loss, acc = model.evaluate(test_gen)
-    print(loss, acc)
 
-    print("Predicting on eval. data...")
-    probs = model.predict(test_gen)
-    probs = np.argmax(probs, axis=1).flatten() # Retain the indicies for the highest prob. classes.
+    if not gridsearch:
+        print(loss, acc)
 
-    print("Exported figures to ../images/temp.")
-    generateFigures(mdlname, hist, test_gen.classes.copy(), probs)
+    if gridsearch:
+        return loss, acc
+    else:
+        print("Predicting on eval. data...")
+        probs = model.predict(test_gen)
+        probs = np.argmax(probs, axis=1).flatten() # Retain the indicies for the highest prob. classes.
 
-    print("Saving model...")
-    model.save('../exported-models/{}'.format(mdlname))
+        print("Exported figures to ../images/temp.")
+        generateFigures(mdlname, hist, test_gen.classes.copy(), probs)
+
+        print("Saving model...")
+        model.save('../exported-models/{}'.format(mdlname))
+
+def gridSearchOptimize(mdlname, verbose=True):
+    K.clear_session() # Clear any previous session!
+    iteration = 1
+    res = []
+
+    # Hyperparams
+    batch_sizes = [8, 16, 32]
+    optimizers = [RMSprop, Adam, SGD]
+    learn_rates = [0.001, 0.003, 0.01, 0.1]    
+    dropouts = [0, 0.1, 0.2]
+    label_smoothening = [0, 0.1, 0.2]
+    regularizers = [l1, l2]
+    regularizer_values = [0.0001, 0.001, 0.01]
+    allow_training = [False, True]
+    start_time = time.time()
+
+    print("Started hyperparameter tuning...")
+    for batchSize in batch_sizes:
+        for optimizer in optimizers:
+            for learnRate in learn_rates:
+                for dropOutRate in dropouts:
+                    for lblSmooth in label_smoothening:
+                        for reg in regularizers:
+                            for regVal in regularizer_values:
+                                for allowTuning in allow_training:
+                                    loop_time = time.time()
+                                    loss, acc = createModel(mdlname, batchSize, 4, optimizer, learnRate, dropOutRate, lblSmooth, reg, regVal, allowTuning, True)
+                                    res.append([
+                                        loss, acc, batchSize, optimizer, learnRate, dropOutRate, lblSmooth, reg, regVal, allowTuning
+                                    ])
+                                    if verbose and ((iteration % 4) == 0):
+                                        print(iteration, "Processed ---> {:.3f} LOSS, {:.3f} ACCU.".format(loss, acc))
+                                        print("Time elapsed for this iter:", (time.time() - loop_time), 'sec.')
+                                    iteration += 1
+                                    K.clear_session() # Clear memory used. Proceed to next!
+
+    end_time = time.time()
+    print("Finished hyperparameter tuning, time elapsed:", (end_time - start_time), 'sec.')
+
+    if len(res) == 0:
+        print("No results were recorded?!")
+        return
+
+    res.sort(key = lambda x: x[0]) # Sort on LOSS, low->high
+    print("Writing results to ../models/{}/stat.csv".format(mdlname))
+    with open('../models/{}/stat.csv'.format(mdlname), 'w') as f:
+        for obj in res:
+            s = ','.join([str(v) for v in obj])
+            f.write('{}\n'.format(s))
+
+    print("Finished hyperparameter tuning!")
+    print("Fetching best loss and acc model for training...")
+
+    mdl = res[0][2:]
+    createModel('{}_loss'.format(mdlname), mdl[0], 4, mdl[1], mdl[2], mdl[3], mdl[4], mdl[5], mdl[6], mdl[7], gridsearch=False)
+
+    res.sort(key = lambda x: x[1], reverse=True) # Sort on ACC, high->low
+    mdl = res[0][2:]
+    createModel('{}_acc'.format(mdlname), mdl[0], 4, mdl[1], mdl[2], mdl[3], mdl[4], mdl[5], mdl[6], mdl[7], gridsearch=False)
 
 if __name__ == "__main__":
     print("TF version:", tf.__version__)
@@ -172,11 +248,10 @@ if __name__ == "__main__":
     if len(listGPUs) == 0:
         print("No GPUs available, terminating!")
     else:
-        createModel(
-            'mobilenet', # Name of this mdl
-            'https://tfhub.dev/google/imagenet/mobilenet_v3_large_100_224/feature_vector/5', # URL for feat. vec. (pre-trained mdl)
-            16, # Batch Size
-            (224, 224) # Target Size
-        )
+        if len(sys.argv) > 1: # Start grid search logic...
+            print("Starting grid search, optimizing...")
+            gridSearchOptimize('mobilenet_opt')
+        else:
+            createModel('mobilenet', BATCH_SIZE)
         print("Finished training model!")
         
